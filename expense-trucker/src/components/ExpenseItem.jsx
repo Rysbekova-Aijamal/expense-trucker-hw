fa544f339573024b938813cb21b4bc14946569d9
Рысбекова  Айжамал <rysbekovaajzamal@MacBook-Air-Rysbekova.local>
2022-11-22 14:33:35 +0600

    add ExpenseItem and ExpenseDate components

---
+++ expense-trucker/src/components/ExpenseItem.jsx
@@ -0,0 +1,19 @@
+import './ExpenseItem.css'
+
+const ExpenseItem = (props) => {
+    const expenseTitle = props.items.title;
+    const expenseDate = props.items.date;
+    const expensePrice = props.items.price;
+   return (
+        <div className='expense-item'>
+            {/* <expenseDate expenseDate={expenseDate}/> */}
+           {/* <ExpenseDate expenseDate ={expenseDate}/> */}
+           <div>{expenseDate.toString()}</div>
+           <h2 className='expense-item__description'>{expenseTitle}</h2>
+            
+            <div className='expense-item__price'>${expensePrice}</div>
+        </div>
+    )
+};
+
+export default ExpenseItem
